b1449db357584e8102e2609105e0f657507376f6
ahmedraafat99 <ahmedraafat4954@gmail.com>
2024-10-18 12:51:30 +0300

    class calc

--- pthon for ml/tasks/task0.py
+++ pthon for ml/tasks/task0.py
@@ -1,12 +1,18 @@
-class A():
-    def do_this(self):
-        print("in A")
-class B(A):
-     pass
-class C():
-    def do_this(self):
-        print("in C")
-class D(C,A):
-    pass              
-x=D()
-x.do_this()
+class calc():
+    def __init__(self,n1,n2):
+        self.n1=n1
+        self.n2=n2
+        
+    def sum(self):
+      return self.n1+self.n2 
+    def sub(self):
+        return self. n1-self.n2
+    def div(self):
+        return self. n1/self.n2
+    def mul(self):
+        return self. n1*self.n2
+    
+    def power(self):
+        return self. n1**self.n2
+c1= calc(6,7)
+print(c1.sum())    
